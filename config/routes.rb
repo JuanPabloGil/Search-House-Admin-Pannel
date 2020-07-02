8c84a45a213308fb08fc642622f3d04ac963fa35
Juan Pablo <jpablomgil@gmail.com>
2020-07-02 16:58:51 -0500

    Create User Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'static#index'
+  devise_for :users
+  resources :houses
 
   get 'v1/houses', to:'houses#api'
 
   # get '*page', to: 'static#index', constrains: ->(req) do 
   #   !req.xhr? && req.format.html?
   # end
-
-  resources :houses
-  root 'static#index'
-  
 end
