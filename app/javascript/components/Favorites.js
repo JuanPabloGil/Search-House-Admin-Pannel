94e219d293a851d5634595577f1595be3ee01153
Juan Pablo <jpablomgil@gmail.com>
2020-07-10 11:42:03 -0500

    Refactor components to functional components

--- app/javascript/components/Favorites.js
+++ app/javascript/components/Favorites.js
@@ -1,21 +1,19 @@
 import React from "react"
 import Houses from './Houses'
 import PropTypes from "prop-types"
-class Favorites extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    const { houses } = this.props
-    const { favorites } = this.props
 
-    return (
-      <div>
-        <h1 className="text-center">My favorite houses</h1>
-        {houses.map((house, i) => <Houses key={`${i}${house.id}`} data={house} favorites={favorites} />)}
-      </div>
-    );
-  }
+const Favorites = props => {
+
+  const { houses } = props
+  const { favorites } = props
+
+  return (
+    <div>
+      <h1 className="text-center">My favorite houses</h1>
+      {houses.map((house, i) => <Houses key={`${i}${house.id}`} data={house} favorites={favorites} />)}
+    </div>
+  );
+
 }
 
 export default Favorites
--- app/javascript/components/Houses.js
+++ app/javascript/components/Houses.js
@@ -3,42 +3,40 @@ import { Link } from 'react-router-dom';
 import houseImage from '../../assets/images/house-image.jpg';
 // import PropTypes from "prop-types"
 
-class Houses extends React.Component {
+const Houses = (props) => {
 
-  render() {
-    const { data } = this.props
-    const { title, price, id, category } = data
-    const { favorites } = this.props
+  const { data } = props
+  const { title, price, id, category } = data
+  const { favorites } = props
 
-    return (
-      <div className="col-md-6 col-lg-4">
-        <div className="shadow p-3 mb-5 bg-white rounded">
-          <img src={houseImage} className="img-fluid" alt="House" />
-          <hr />
+  return (
+    <div className="col-md-6 col-lg-4">
+      <div className="shadow p-3 mb-5 bg-white rounded">
+        <img src={houseImage} className="img-fluid" alt="House" />
+        <hr />
 
-          <div className="row">
-            <div className="col-8">
-              <Link
-                to={
-                  {
-                    pathname: `/house/${id}`,
-                    state: { data, favorites },
-                  }
+        <div className="row">
+          <div className="col-8">
+            <Link
+              to={
+                {
+                  pathname: `/house/${id}`,
+                  state: { data, favorites },
                 }
-              >
-                <h2 className="h6 text-dark"> <strong>{title}</strong></h2>
-                <span className="badge badge-secondary mb-3"> {category} </span>
-              </Link>
-            </div>
-            <div className="col-4">
-              <p className="text-right"> $ {price} <span className="text-muted"> / Month </span></p>
-            </div>
-
+              }
+            >
+              <h2 className="h6 text-dark"> <strong>{title}</strong></h2>
+              <span className="badge badge-secondary mb-3"> {category} </span>
+            </Link>
           </div>
+          <div className="col-4">
+            <p className="text-right"> $ {price} <span className="text-muted"> / Month </span></p>
+          </div>
+
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Houses
